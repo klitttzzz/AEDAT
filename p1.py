e95752fdd7bda3864cc042c330a2b60d9c915c07
klittzz <lindernest296@gmail.com>
2025-09-23 16:19:09 +0200

    Funciones search_all y majority_search  hechas

--- p1.py
+++ p1.py
@@ -6,6 +6,26 @@ def search_all(lst, vy):
     
     return lstaux
 
+def majority_search(lst):
+    candidate = None
+    count = 0
+
+    for num in lst:
+        if count == 0:
+            candidate = num
+            count = 1
+        elif candidate == num:
+            count += 1
+        else:
+            count -= 1
+
+    if lst.count(candidate) > len(lst) // 2:
+        return candidate
+    else:
+        return None
+
 lst = [1,2,2,1]
 lstaux = search_all(lst, 2)
+naux = majority_search(lst)
 print(lstaux)
+print(naux)
